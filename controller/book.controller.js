eb03a79c54b08700eee456b82c5c54c9a44d2fb7
AnStarseed98 <an.dtStarseed>
2020-03-01 21:42:10 +0700

    sort book

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -2,7 +2,7 @@ const book = require("../models/book.model");
 
 module.exports = {
   index: async (req,res,next) => {
-      let getBook = await book.find();
+      let getBook = await book.find().sort('-view');
       return res.send(getBook);
   },
 };
